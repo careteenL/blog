e4aebb23472ff596eef1949161285c2daca268c2
Careteen <15074806497@163.com>
2024-03-27 17:31:12 +0800

    docs: ✏️ add comment

--- tree/Typescript/1.type.ts
+++ tree/Typescript/1.type.ts
@@ -1,5 +1,7 @@
 /**
  * @desc 获取 props 中类型为 Function 的 键
+ * @example GetKeyByValueType<SomeProps, Function>
+ *  => 'c' | 'd'
  * @knowledge 泛型 never keyof extends in
  */
 interface SomeProps {
@@ -11,8 +13,7 @@ interface SomeProps {
 type GetKeyByValueType<T, U> = {
   [K in keyof T]: T[K] extends U ? K : never
 }[keyof T]
-// 返回 'c' | 'd'
-type FunctionPropNames = GetKeyByValueType<SomeProps, Function>
+type GetKeyByValueTypeEg = GetKeyByValueType<SomeProps, Function>
 
 /**
  * @desc 不确定传参或返回值时，都使用 unknown 而非 any
@@ -30,17 +31,20 @@ function resultValueBySome(val: unknown) {
 
 /**
  * @desc 联合类型
+ * @example UnionInferType<{ a: string; b: boolean }>
+ *  => string | boolean
  * @knowledge infer
  */
 type UnionInferType<T> = T extends { a: infer U; b: infer U } ? U : never
-type Foo1 = UnionInferType<{ a: string; b: boolean }>
-
+type UnionInferTypeEg = UnionInferType<{ a: string; b: boolean }>
 
 /**
  * @des 后者覆盖前者的同名属性
  * 1. 找到 T 有，但 U 没有的 { other: boolean} Exclude
  * 2. 找到 T 有 U 也有的 key key1 在 U 中 pick 出来 {key1: string}
  * 3. 合并 1 和 2
+ * @example Overwrite<{ key1: number, other: boolean }, { key1: string, key3: number }>
+ *  => { key1: string, other: boolean }
  * @knowledge Pick Exclude Extract keyof 
  */
 type Diff<T extends object, U extends object> = Pick<T, Exclude<keyof T, keyof U>>
